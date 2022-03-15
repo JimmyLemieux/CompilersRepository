ce1457f3f962d71d53064c23a3baee9a4f0d6fa8
mustafa <malobaid@uoguelph.ca>
2022-03-15 17:21:53 -0400

    modified the CM file to handle the -s command and added SemanticAnaylzer

--- SampleParser/CM.java
+++ SampleParser/CM.java
@@ -7,18 +7,37 @@ import java.io.*;
 import absyn.*;
    
 class CM {
-  public final static boolean SHOW_TREE = true;
+  public static boolean SHOW_TREE = false;
+  public static boolean SHOW_SEMATIC = false;
   static public void main(String argv[]) {    
     /* Start the parser */
     try {
+      if (argv[1].equals("-a")) {
+        SHOW_TREE = true;
+      }
+      else if (argv[1].equals("-s")) {
+        SHOW_SEMATIC = true;
+      }
+      else{
+        System.out.println("Error: Please add -a flag for the abstract syntax tree or -s for the symantic analyzer tree!");
+      }
       parser p = new parser(new Lexer(new FileReader(argv[0])));
       Absyn result = (Absyn)(p.parse().value);      
       if (SHOW_TREE) {
          System.out.println("The abstract syntax tree is:");
          ShowTreeVisitor visitor = new ShowTreeVisitor();
          if (result != null)
-          result.accept(visitor, 0); 
+          result.accept(visitor, 0);
       }
+      else if(SHOW_SEMATIC){
+        System.out.println("The Semantic analyzer tree is:");
+        SemanticAnalyzer visitor = new SemanticAnalyzer();
+        System.out.println("Entering the global scope:");
+        if (result != null)
+          result.accept(visitor,0);
+        System.out.println("Leaving the global scope");
+      }
+    
     } catch (Exception e) {
       /* do cleanup here -- possibly rethrow e */
       e.printStackTrace();
---
+++ SampleParser/SemanticAnalyzer.java
@@ -0,0 +1,73 @@
+import absyn.*;
+
+public class SemanticAnalyzer implements AbsynVisitor{
+    
+  public void visit( ExpList expList, int level ) {
+  }
+
+  public void visit( AssignExp exp, int level ) {
+  }
+
+  public void visit( IfExp exp, int level ) {
+  }
+
+  public void visit( IntExp exp, int level ) {
+  }
+
+  public void visit( OpExp exp, int level ) {
+  }
+
+  public void visit( VarExp exp, int level ) {
+  }
+
+  public void visit( WriteExp exp, int level ) {
+  }
+  
+  public void visit( DeclarationList decList, int level ) {
+  }
+
+  public void visit( SimpleVar var, int level ) {
+  }
+
+  public void visit( VarAssignExp varAssign, int level) {
+  }
+
+  public void visit(SimpleIndexVar var, int level) {
+  }
+
+  public void visit(ArrayDec exp, int level) {
+  }
+
+  public void visit(CallingExp exp, int level) {
+  }
+  
+  public void visit(CompoundExp exp, int level) {
+  }
+
+  public void visit(FunctionDec exp, int level) {
+  }
+
+  public void visit(ReturnExp exp, int level) {
+  }
+
+  public void visit(VariableDeclaration exp, int level) {
+  }
+
+  public void visit(TypeName exp, int level) {
+  }
+
+  public void visit(VarDecList expList, int level) {
+  }
+
+  public void visit(WhileExp exp, int level) {
+  }
+
+  public void visit(SimpleVarExp exp, int level) {
+  }
+
+  public void visit(VarDecExp vExp, int level) {
+  }
+
+  public void visit(ArrayAssignExp exp, int level) {
+  }
+}
