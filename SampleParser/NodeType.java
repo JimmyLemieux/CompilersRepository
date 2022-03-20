cb9229f81bef84d92c08c96312ef90fb8e1c9fdd
James Lemieux <jlemie03@fred.socs.uoguelph.ca>
2022-03-20 19:23:57 -0400

    added symbol table, insert, lookup and delete

--- SampleParser/NodeType.java
+++ SampleParser/NodeType.java
@@ -2,6 +2,14 @@
   Created by: James Lemieux and Mustafa Al-Obaidi
   File Name: NodeType.java
 */
-public class NodeType{
+public class NodeType {
+  public String name;
+  public String def;
+  public int level;
 
+  public NodeType(String name, String def, int level) {
+    this.name = name;
+    this.def = def;
+    this.level = level;
+  }
 }
--- SampleParser/SemanticAnalyzer.java
+++ SampleParser/SemanticAnalyzer.java
@@ -1,77 +1,341 @@
 /*
   Created by: James Lemieux and Mustafa Al-Obaidi
-  File Name: SemanticAnalyzer.java
+  File Name: showTreeVisitor.java
 */
 import absyn.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Set;
 
-public class SemanticAnalyzer implements AbsynVisitor{
+public class SemanticAnalyzer implements AbsynVisitor {
+
+  HashMap<String, ArrayList<NodeType>> table;
+  int globalLevel = 0;
+
+  public SemanticAnalyzer() {
+    table = new HashMap<String, ArrayList<NodeType>>();
+  }
+
+  private void insert(NodeType node) {
+    if (!table.containsKey(node.name)) {
+      ArrayList<NodeType> list = new ArrayList<NodeType>();
+      list.add(0, node);
+      table.put(node.name, list);
+    } else {
+      ArrayList<NodeType> c = table.get(node.name);
+      c.add(0, node);
+      table.put(node.name, c);
+    }
+  }
+
+  private NodeType lookup(NodeType node) {
+    if (!table.containsKey(node.name)) {
+      return null;
+    }
+    ArrayList<NodeType> c = table.get(node.name);
+    return c.get(c.size() - 1);
+  }
+
+  private void delete(int level) {
+    Set<String> keys = table.keySet();
+    ArrayList<String> keysToDelete = new ArrayList<String>();
     
+    for (String key : keys) {
+      ArrayList<NodeType> c = table.get(key);
+      NodeType current = c.get(0);
+      if (current.level == level) {
+        // remove the key
+        c.remove(current);
+        if (c.isEmpty()) {
+          // remove this from the table
+          keysToDelete.add(key);
+        }
+      }
+    }
+    table.keySet().removeAll(keysToDelete);
+  }
+
+  // do later
+  public void printLevel(int level) {
+
+  }
+
+  final static int SPACES = 4;
+
+  private void indent( int level ) {
+    for( int i = 0; i < level * SPACES; i++ ) System.out.print( " " );
+  }
+
   public void visit( ExpList expList, int level ) {
+    while( expList != null ) {
+      if (expList.head != null) {
+        expList.head.accept( this, level );
+      } else {
+        System.out.println("Error: Invalid Expression List. row: " + expList.row + " col: " + expList.col);
+      }
+      expList = expList.tail;
+    } 
   }
 
   public void visit( AssignExp exp, int level ) {
+    indent( level );
+    System.out.println( "AssignExp:" );
+    level++;
+    exp.lhs.accept( this, level );
+    exp.rhs.accept( this, level );
   }
 
   public void visit( IfExp exp, int level ) {
+    indent( level );
+    System.out.println( "IfExp:" );
+    level++;
+    if (exp.test != null) {
+      exp.test.accept( this, level );
+    } else {
+      indent(level);
+      System.out.println("Error: No expression given for if condition! row: " + exp.row + " col: " + exp.col);
+    }
+    exp.thenpart.accept( this, level );
+    if (exp.elsepart != null )
+       exp.elsepart.accept( this, level );
   }
 
   public void visit( IntExp exp, int level ) {
+    indent( level );
+    System.out.println( "IntExp: " + exp.value ); 
   }
 
   public void visit( OpExp exp, int level ) {
+    indent( level );
+    System.out.print( "OpExp:" ); 
+    switch( exp.operation ) {
+      case OpExp.PLUS:
+        System.out.println( " + " );
+        break;
+      case OpExp.MINUS:
+        System.out.println( " - " );
+        break;
+      case OpExp.TIMES:
+        System.out.println( " * " );
+        break;
+      case OpExp.OVER:
+        System.out.println( " / " );
+        break;
+      case OpExp.EQ:
+        System.out.println( " = " );
+        break;
+      case OpExp.LT:
+        System.out.println( " < " );
+        break;
+      case OpExp.GT:
+        System.out.println( " > " );
+        break;
+      case OpExp.MUL:
+        System.out.println(" * ");
+        break;
+      case OpExp.LTEQ:
+        System.out.println(" <= ");
+        break;
+      case OpExp.NOTEQ:
+        System.out.println(" != ");
+        break;
+      case OpExp.GTEQ:
+        System.out.println(" >= ");
+        break;
+      default:
+        indent(level);
+        System.out.println( "Error: Unrecognized operator at row: " + exp.row + " and col: " + exp.col);
+    }
+    level++;
+    exp.left.accept( this, level );
+    exp.right.accept( this, level );
   }
 
   public void visit( VarExp exp, int level ) {
+    indent( level );
+    System.out.println( "VarExp: " + exp.name );
   }
 
   public void visit( WriteExp exp, int level ) {
+    indent( level );
+    System.out.println( "WriteExp:" );
+    exp.output.accept( this, ++level );
   }
   
   public void visit( DeclarationList decList, int level ) {
+    while( decList != null ) {
+      if (decList.head != null) {
+        decList.head.accept( this, level );
+      } else {
+        indent(level);
+        System.out.println("Error: Invalid Declaration List. row: " + decList.row + " col: " + decList.col);
+      }
+      decList = decList.tail;
+    }
   }
 
   public void visit( SimpleVar var, int level ) {
+    indent( level );
+    System.out.println(" Simple Variable: " + var.varName);
   }
 
   public void visit( VarAssignExp varAssign, int level) {
+    indent( level );
+    System.out.println( "VarAssignExp:" );
+    level++;
+    varAssign.lhs.accept( this, level );
+    varAssign.rhs.accept( this, level );
   }
 
   public void visit(SimpleIndexVar var, int level) {
+    indent( level );
+    System.out.println("Simple Index Var:" + var.varName);
+    level++;
+    var.exp.accept(this, level);
   }
 
   public void visit(ArrayDec exp, int level) {
+    indent (level);
+    if (exp.arraySize != null){
+        System.out.print( "ArrayDec: " + exp.arrayName + "[");
+        System.out.println(exp.arraySize.value + "]: ");
+    }
+    else
+        System.out.println("Error: Invalid Array Index! row: " + exp.row + " col: " + exp.col);
+  
+    if (exp.type != null) {
+      level++;
+      indent (level);
+      System.out.print("Array Type: ");
+      if (exp.type.type == 0)
+          System.out.println("Void");
+      else if (exp.type.type == 1)
+          System.out.println("Int");
+    } else {
+      indent(level);
+      System.out.println("Error: Symbol not found! row: " + exp.row + " col: " + exp.col);
+    }
   }
 
   public void visit(CallingExp exp, int level) {
+    indent( level );
+    System.out.println( "CallExp: " + exp.funName);
+    level++;
+    ExpList args = exp.args;
+    while (args != null) {
+        args.head.accept(this, level);
+        args = args.tail;
+    }
   }
   
   public void visit(CompoundExp exp, int level) {
+    indent (level);
+    System.out.println( "CompoundExp: ");
+    level++;
+    VarDecList dds = exp.decl;
+    while (dds != null) {
+        dds.head.accept(this, level);
+        dds = dds.tail;
+    }
+    ExpList exps = exp.expl;
+    while (exps != null) {
+        exps.head.accept(this, level);
+        exps = exps.tail;
+    }
   }
 
   public void visit(FunctionDec exp, int level) {
+    indent (level);
+    System.out.print( "FunctionDec: " + exp.funName + " - ");
+    if (exp.type.type == 0)
+        System.out.println("Void");
+    else if (exp.type.type == 1)
+        System.out.println("Int");
+    level++;
+    VarDecList parms = exp.param;
+    while (parms != null) {
+        if (parms.head == null){
+          System.out.println("Invalid Declaration list! row : " + exp.row + " col: " + exp.col);
+        }
+        else{
+          parms.head.accept(this, level);
+        }
+        parms = parms.tail;
+    }
+    exp.funBody.accept(this, level);
   }
 
   public void visit(ReturnExp exp, int level) {
+    indent (level);
+    System.out.println( "ReturnExp: ");
+    level++;
+    if (exp.expr != null)
+        exp.expr.accept(this, level);
   }
 
   public void visit(VariableDeclaration exp, int level) {
+    indent (level);
+    System.out.print( "Variable Declaration: " + exp.sname + ": ");
+    if (exp.type.type == 0)
+        System.out.println("Void");
+    else if (exp.type.type == 1)
+        System.out.println("Int");
   }
 
   public void visit(TypeName exp, int level) {
+    indent (level);
+    System.out.print( "TypeName: ");
+    if (exp.type == 0)
+        System.out.println("Void");
+    else if (exp.type == 1)
+        System.out.println("Int");
   }
 
   public void visit(VarDecList expList, int level) {
+    while( expList != null ) {
+        if (expList.head == null){
+          System.out.println("Invalid Declaration list! row : " + expList.row + " col: " + expList.col);
+        }
+        expList.head.accept( this, level );
+        expList = expList.tail;
+    }
   }
 
   public void visit(WhileExp exp, int level) {
+    indent (level);
+    System.out.println( "WhileExp: ");
+    level++;
+    if (exp.test != null)
+      exp.test.accept(this, level);
+    else {
+      indent(level);
+      System.out.println("Error: No expression given for while condition! row: " + exp.row + " col: " + exp.col);
+    }
+    exp.exps.accept(this, level);
   }
 
   public void visit(SimpleVarExp exp, int level) {
+    indent(level);
+    System.out.println(" SimpleVarExp: ");
+    level++;
+    exp.var.accept(this, level);
   }
 
   public void visit(VarDecExp vExp, int level) {
+    indent(level);
+    level++;
+    System.out.print( "VariableDeclaratonExp: " + vExp.varName + " - ");
+    vExp.name.accept(this, level);
+    vExp.exp.accept(this, level);
+
   }
 
   public void visit(ArrayAssignExp exp, int level) {
+    indent(level);
+    level++;
+    System.out.println( "ArrayAssignExp:" );
+    exp.lhs.accept(this, level);
+    exp.index.accept(this, level);
+    exp.rhs.accept(this, level);
   }
 }
